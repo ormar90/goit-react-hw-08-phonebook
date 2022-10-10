7210221b7c26c518a6c32bfa1d9208b4715c2044
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-10-10 19:49:35 +0300

    fixed bug with regex validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -21,7 +21,7 @@ let schema = yup.object().shape({
     number: yup
         .string()
         .matches(
-            /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+            /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/,
             "Phone number must be digits and can contain spaces, dashes, parentheses and can start with +")
         .required()  
 });
