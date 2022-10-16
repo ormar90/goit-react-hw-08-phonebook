5f7bcd168de05686775753d4b7f04f77d993efef
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-10-16 12:34:41 +0300

    fixed bug with empty local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,25 @@ import { Wrapper } from "./App.styled";
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
     filter: '',
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
     }
   }
 
   componentDidMount() {
-    this.setState({ contacts: JSON.parse(localStorage.getItem("contacts"))})
+    if (JSON.parse(localStorage.getItem("contacts"))) {
+      this.setState({ contacts: JSON.parse(localStorage.getItem("contacts"))})
+    }    
   }
 
   formSubmit = (data) => {
