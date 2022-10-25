e56221f8655588507fbf0ec0ff7a6fb8353a42c3
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-10-25 22:10:48 +0300

    fixed bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,12 @@ import { ContactsList } from "./ContactList/ContactsList";
 import { Wrapper } from "./App.styled";
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    if (JSON.parse(localStorage.getItem("contacts"))) {
-      return JSON.parse(localStorage.getItem("contacts"));
+  const [contacts, setContacts] = useState(initialStateContacts());
+
+  function initialStateContacts() {
+    const data = JSON.parse(localStorage.getItem("contacts"))
+    if (data) {
+      return data;
     }
     return [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -16,7 +19,7 @@ export const App = () => {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ]
-  });
+  }
 
   const [filter, setFilter] = useState('');
 
