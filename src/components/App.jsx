3ff8d5d33e011de8a0c40e35c0a8a845c9d78106
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-12-18 20:18:47 +0200

    fixed deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,8 @@
-import { ContactForm } from "./ContactForm/ContactForm";
-import { Filter } from "./Filter/Filter";
-import { ContactsList } from "./ContactList/ContactsList";
-import { Wrapper } from "./App.styled";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { selectContacts } from "redux/Contacts/selectors";
 import { refreshUser } from "redux/Auth/operations";
 import { Routes, Route } from "react-router-dom"
 import { Layout } from "./Layout";
-import { selectIsRefreshing } from "redux/Auth/selectors";
 import { Contacts } from "pages/Contacts";
 import { Login } from "pages/Login";
 import { Register } from "pages/Register";
@@ -17,8 +11,6 @@ import { PrivateRoute } from "./PrivateRoute";
 import { RestrictedRoute } from "./RestrictedRoute";
 
 export const App = () => {
-  const contacts = useSelector(selectContacts);
-  const isRefreshing = useSelector(selectIsRefreshing);
   const dispatch = useDispatch();
 
   useEffect(() => {
